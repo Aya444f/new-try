bd0c93d9e701e45f0bf6cb3a3707a922280ba9e7
Aya Mahmoud <ayab18102018@gmail.com>
2024-12-22 01:24:45 +0200

    Add files via upload

---
+++ try.js
@@ -0,0 +1,17 @@
+//document.getElementById("count").innerText = 5;
+//let count = 5
+//console.log(count);
+//let myAge = 28;
+//onsole.log(myAge);
+let count = 55 / 5;
+console.log(count);
+const amount = 300 * 4;
+console.log(amount);
+let comers = 40;
+let nepass = 50;
+let pass = comers + nepass;
+console.log(pass);
+let myAge = 28;
+let humanDogratio = 7;
+let myDogage = myAge * humanDogratio;
+console.log(myDogage)
